8fe833fdaf514907eed72ac6af02262699481b11
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2020-11-28 23:17:58 -0500

    routes done, TODO: testing

--- song-microservice/src/main/java/com/csc301/songmicroservice/SongDalImpl.java
+++ song-microservice/src/main/java/com/csc301/songmicroservice/SongDalImpl.java
@@ -1,5 +1,7 @@
 package com.csc301.songmicroservice;
 
+import java.util.Map;
+
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -12,6 +14,13 @@ public class SongDalImpl implements SongDal {
 
 	private final MongoTemplate db;
 
+	/**
+	 * readability
+	 */
+	private final DbQueryExecResult OK = DbQueryExecResult.QUERY_OK;
+	private final DbQueryExecResult ERR = DbQueryExecResult.QUERY_ERROR_GENERIC;
+	private final DbQueryExecResult ERR404 = DbQueryExecResult.QUERY_ERROR_NOT_FOUND;
+
 	@Autowired
 	public SongDalImpl(MongoTemplate mongoTemplate) {
 		this.db = mongoTemplate;
@@ -19,31 +28,51 @@ public class SongDalImpl implements SongDal {
 
 	@Override
 	public DbQueryStatus addSong(Song songToAdd) {
-		// TODO Auto-generated method stub
-		return null;
+		boolean valid = songToAdd.getSongName() != null && songToAdd.getSongAlbum() != null
+				&& songToAdd.getSongArtistFullName() != null;
+		Song inserted = valid ? db.insert(songToAdd, "songs") : null;
+		DbQueryStatus status = new DbQueryStatus("POST", inserted != null ? this.OK : this.ERR);
+		if (inserted != null)
+			status.setData(inserted.getJsonRepresentation());
+		return status;
 	}
 
 	@Override
 	public DbQueryStatus findSongById(String songId) {
-		// TODO Auto-generated method stub
-		return null;
+		Song found = db.findById(new ObjectId(songId), Song.class);
+		DbQueryStatus status = new DbQueryStatus("GET", found != null ? this.OK : this.ERR404);
+		status.setData(found.getJsonRepresentation());
+		return status;
 	}
 
 	@Override
 	public DbQueryStatus getSongTitleById(String songId) {
-		// TODO Auto-generated method stub
-		return null;
+		Song found = db.findById(new ObjectId(songId), Song.class);
+		DbQueryStatus status = new DbQueryStatus("GET", found != null ? this.OK : this.ERR404);
+		if(found != null)
+			status.setData(found.getSongName());
+		return status;
 	}
 
 	@Override
 	public DbQueryStatus deleteSongById(String songId) {
-		// TODO Auto-generated method stub
-		return null;
+		Song found = (Song) this.findSongById(songId).getData();
+		DbQueryStatus status = new DbQueryStatus("DELETE", found == null ? this.ERR404 : this.OK);
+		if (found != null) {
+			db.remove(found, "songs");
+		}
+		return status;
 	}
 
 	@Override
 	public DbQueryStatus updateSongFavouritesCount(String songId, boolean shouldDecrement) {
-		// TODO Auto-generated method stub
-		return null;
+		Song found = db.findById(new ObjectId(songId), Song.class);
+		if (found != null) {
+			long count = found.getSongAmountFavourites() + (shouldDecrement ? -1 : 1);
+			found.setSongAmountFavourites(count);
+			found = db.save(found);
+		}
+		DbQueryStatus status = new DbQueryStatus("PUT", found == null ? this.ERR404 : this.OK);
+		return status;
 	}
 }
